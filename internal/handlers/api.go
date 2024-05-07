d6569ae9d277a141e849299b0581f79cc4c95b8b
Arish Imam <arish.imam@gmail.com>
2024-05-07 07:26:24 -0700

    rename

---
+++ internal/handlers/api.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"github.com/go-chi/chi"
+	chimiddle "github.com/go-chi/chi/middleware"
+	//"github.com/arishimam/goapi/internal/middleware"
+)
+
+// starting this with uppercase means it can be imported elsewhere. Lowercase = private
+func Handler(r *chi.Mux) {
+	// Global middleware
+	r.Use(chimiddle.StripSlashes)
+	r.Route("/account", func(router chi.Router) {
+		// Middleware for /account route
+		router.Use(middleware.Authorization)
+
+		router.Get("/coins", GetCoinBalance)
+	})
+}
